f2c2f20bed4ce8d1dbb989ca14cb1fc3c67201e1
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-06-25 22:55:51 -0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const mysql = require("mysql");
 const cors = require("cors"); 
 
+const CorreioController = require('./controllers/CorreioController');
+
 const app = express();
-
-
-
 
 app.use(express.json());
 app.use(cors());
 
-app.use(cors());
+app.use('/correio', CorreioController);  
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000');
-});
+}); 
