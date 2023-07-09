74f7b0e81f37e0364393d427264163b18b9a822c
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-08 23:03:10 -0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const express = require("express");
-const cors = require("cors"); 
-
-const router = require("./routes/api");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(router);  
+const correioRoutes = require('./routes/correio');
+const userRoutes = require('./routes/user');
+
+app.use('/api/correio-elegante', correioRoutes);
+app.use('/api/correio-elegante/user', userRoutes);
 
 const swaggerSetup = require('./swagger');
 swaggerSetup(app);
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000');
-}); 
+});
