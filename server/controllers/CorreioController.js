5d3f38b6efb46425120fa833f797fbee8ef9748a
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-08 23:03:28 -0300

    changes

--- server/controllers/CorreioController.js
+++ server/controllers/CorreioController.js
@@ -1,106 +1,110 @@
 const db = require('../configs/database');
 
 class CorreioController {
+  /**
+   * Obtém todos os correios elegantes.
+   * @param {object} req - Objeto de requisição.
+   * @param {object} res - Objeto de resposta.
+   * @returns {object} - Lista de correios elegantes.
+   */
+  async selectAll(req, res) {
+    try {
+      const connection = await db.getConnection();
 
-  // Método para selecionar todos os dados da tabela
-  selectAll(req, res) {
-    db.getConnection((err, connection) => {
-      if (err) {
-        console.error('Erro ao obter conexão do pool:', err);
-        return res.status(500).json({ error: 'Erro interno do servidor' });
-      }
+      const query = 'CALL GetMessage()';
+      const [results] = await connection.query(query);
 
-      connection.query('CALL GetMessage();', (err, results) => {
-        connection.release(); 
+      connection.release();
 
-        if (err) {
-          console.error('Erro ao executar a consulta:', err);
-          return res.status(500).json({ error: 'Erro interno do servidor' });
-        }
-
-        res.json(results);
-      });
-    });
+      res.json(results);
+    } catch (error) {
+      console.error('Erro ao executar a consulta:', error);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
   }
 
-  // Método para inserir dados na tabela
-  insertData(req, res) {
-    const { user_id, content } = req.body;
-    const created_at = new Date();
+  /**
+   * Insere um novo correio elegante.
+   * @param {object} req - Objeto de requisição.
+   * @param {object} res - Objeto de resposta.
+   * @returns {object} - Mensagem de sucesso.
+   */
+  async insertData(req, res) {
+    try {
+      const { userId, content, nomeDestinatario, serieEscolhida, cursoEscolhido, periodo, dica, corBilhete, formaCartinha, formaPagamento } = req.body;
+      const createdAt = new Date();
 
-    db.getConnection((err, connection) => {
-      if (err) {
-        console.error('Erro ao obter conexão do pool:', err);
-        return res.status(500).json({ error: 'Erro interno do servidor' });
-      }
+      const connection = await db.getConnection();
 
-      const query = 'INSERT INTO message (user_id, content, created_at) VALUES (?, ?, ?)';
-      const values = [user_id, content, created_at];
+      const query = 'INSERT INTO message (user_id, content, created_at, nome_destinatario, serie_escolhida, curso_escolhido, periodo, dica, cor_bilhete, forma_cartinha, forma_pagamento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+      const values = [userId, content, createdAt, nomeDestinatario, serieEscolhida, cursoEscolhido, periodo, dica, corBilhete, formaCartinha, formaPagamento];
 
-      connection.query(query, values, (err, results) => {
-        connection.release();
+      await connection.query(query, values);
 
-        if (err) {
-          console.error('Erro ao executar a consulta:', err);
-          return res.status(500).json({ error: 'Erro interno do servidor' });
-        }
+      connection.release();
 
-        res.json({ message: 'Dados inseridos com sucesso' });
-      });
-    });
+      res.json({ message: 'Dados inseridos com sucesso' });
+    } catch (error) {
+      console.error('Erro ao executar a consulta:', error);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
   }
 
-  // Método para atualizar dados na tabela
-  updateData(req, res) {
-    const { message_id, content } = req.body;
+  /**
+   * Atualiza um correio elegante pelo ID.
+   * @param {object} req - Objeto de requisição.
+   * @param {object} res - Objeto de resposta.
+   * @returns {object} - Mensagem de sucesso.
+   */
+  async updateData(req, res) {
+    try {
+      const { message_id, content } = req.body;
 
-    db.getConnection((err, connection) => {
-      if (err) {
-        console.error('Erro ao obter conexão do pool:', err);
-        return res.status(500).json({ error: 'Erro interno do servidor' });
-      }
+      const connection = await db.getConnection();
 
       const query = 'UPDATE message SET content = ? WHERE message_id = ?';
       const values = [content, message_id];
 
-      connection.query(query, values, (err, results) => {
-        connection.release(); 
+      await connection.query(query, values);
 
-        if (err) {
-          console.error('Erro ao executar a consulta:', err);
-          return res.status(500).json({ error: 'Erro interno do servidor' });
-        }
+      connection.release();
 
-        res.json(values);
-      });
-    });
+      res.json({ message: 'Dados atualizados com sucesso' });
+    } catch (error) {
+      console.error('Erro ao executar a consulta:', error);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
   }
 
-  deleteData(req, res) {
-    const message_id = req.params.message_id;
-  
-    db.getConnection((err, connection) => {
-      if (err) {
-        console.error('Erro ao obter conexão do pool:', err);
-        return res.status(500).json({ error: 'Erro interno do servidor' });
-      }
-  
-      const query = 'UPDATE message SET status = "desativado" WHERE message_id = ?';
+  /**
+   * Desativa um correio elegante pelo ID.
+   * @param {object} req - Objeto de requisição.
+   * @param {object} res - Objeto de resposta.
+   * @returns {object} - Mensagem de sucesso.
+   */
+  async deleteData(req, res) {
+    try {
+      const message_id = req.params.message_id;
+
+      const connection = await db.getConnection();
+
+      const query = 'CALL UpdateMessageStatus(?)';
       const values = [message_id];
-  
-      connection.query(query, values, (err, results) => {
-        connection.release();
 
-        if (err) {
-          console.error('Erro ao executar a consulta:', err);
-          return res.status(500).json({ error: 'Erro interno do servidor' });
-        }
-  
-        res.json(message_id);
-      });
-    });
+      const [results] = await connection.query(query, values);
+
+      connection.release();
+
+      if (results.affectedRows < 0) {
+        return res.status(500).json({ error: 'ID inválido' });
+      }
+
+      res.json({ message: 'Deletado com sucesso' });
+    } catch (error) {
+      console.error('Erro ao executar a consulta:', error);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
   }
-  
 }
 
 module.exports = CorreioController;
