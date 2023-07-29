99950dd1c977064a6bcf7260df0b53e9b248cc69
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-28 21:53:23 -0300

    add

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Register from "../pages/Register";
 import Login from "../pages/Login";
 
 const Router = createBrowserRouter([
