80fcfc72c1a113727df9cee7e85db9dbe89d9c77
Gustavo H. J. <gustavojs417@gmail.com>
2024-03-23 18:28:54 -0300

    feat: add pages -> TypeLetter Show

---
+++ client/src/pages/Admin/TypeLetter/Create/index.jsx
@@ -0,0 +1,15 @@
+const Create = () => {
+  return (
+    <div className="wrapper">
+        <Header type="admin" />
+
+        <main className="container">
+          <h1 className="title">
+            Criar Tipo de Envelope
+           </h1>
+        </main>
+    </div>
+  );
+};
+
+export default Create;
