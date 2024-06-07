d41c649ff406b171599a5623c83ce86740ca2d2c
Gustavo H. J. <gustavojs417@gmail.com>
2024-06-07 17:17:40 -0300

    fix: error in package.json

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const CONFIGS = require('./configs/')
 const app = express();
-import express from 'express';
 
 app.use(cors());
 app.use(express.json());
