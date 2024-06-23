46a46a15c98e85e8e1332ca1f3c52633ba00dd29
Gustavo H. J. <gustavojs417@gmail.com>
2024-06-23 17:29:30 -0300

    feat final tests

--- client/src/pages/Guest/Home/index.jsx
+++ client/src/pages/Guest/Home/index.jsx
@@ -8,70 +8,72 @@ import './style.css';
 const Home = () => {
   return (
     <>
-    <Header type="guest" />
-    <div className="wrapper">
-      
-      <main className="home-guest">
-        
-        <section className="welcome">
-          <article>
-            <h2 className="subtitle">
-              Bem-vindo ao Correio Elegante! 💌
-            </h2>
+      <Header type="guest" />
+      <div className="wrapper">
 
-            <p>
-              Olá! Estamos muito felizes em vê-lo. Aqui, você pode enviar mensagens anônimas cheias de carinho e gentileza.
-            </p>
-            <p>
-              Navegue pelo nosso site, explore as diferentes opções e não hesite em enviar uma mensagem para alguém especial. Lembre-se, suas palavras têm o poder de trazer um sorriso para o rosto de alguém.
-            </p>
-          </article>
+        <main className="home-guest">
 
-          <button className="btn btn-alert">
-            Quero enviar o meu Correio
-          </button>
-        </section>
+          <section className="welcome">
+            <article>
+              <h2 className="subtitle">
+                Bem-vindo ao Correio Elegante! 💌
+              </h2>
 
-        <section className="section-common">
-          <h2 className="subtitle">
-            Confira Nosso Catálogo!
-          </h2>
+              <p>
+                Olá! Estamos muito felizes em vê-lo. Aqui, você pode enviar mensagens anônimas cheias de carinho e gentileza.
+              </p>
+              <p>
+                Navegue pelo nosso site, explore as diferentes opções e não hesite em enviar uma mensagem para alguém especial. Lembre-se, suas palavras têm o poder de trazer um sorriso para o rosto de alguém.
+              </p>
+            </article>
 
-          <p>
-            Dê uma olhada nos nossos serviços exclusivos do Correio Elegante! Oferecemos uma variedade de opções para você expressar seus sentimentos de maneira única e especial. Temos tudo para ajudá-lo a demonstrar seu carinho.
-          </p>
-          <p>
-            Não perca tempo, explore nosso catálogo agora mesmo e encontre o serviço perfeito para fazer o dia de alguém mais brilhante!
-          </p>
+            <button className="btn btn-alert">
+              Quero enviar o meu Correio
+            </button>
+          </section>
 
-          <button className="btn btn-success">
-            Conferir agora!
-          </button>
-        </section>
+          <section className="section-common">
+            <article>
+              <h2 className="subtitle">
+                Confira Nosso Catálogo!
+              </h2>
+
+              <p>
+                Dê uma olhada nos nossos serviços exclusivos do Correio Elegante! Oferecemos uma variedade de opções para você expressar seus sentimentos de maneira única e especial. Temos tudo para ajudá-lo a demonstrar seu carinho.
+              </p>
+              <p>
+                Não perca tempo, explore nosso catálogo agora mesmo e encontre o serviço perfeito para fazer o dia de alguém mais brilhante!
+              </p>
+            </article>
+
+            <button className="btn btn-success">
+              Conferir agora!
+            </button>
+          </section>
 
 
-        <section className="section-common section-report border-warning">
-          <h2 className="subtitle">
-             <i class="fi fi-rr-hexagon-exclamation"></i>
-            <br/><br/>
-            Sua opinião é importante!
-          </h2>
-          <br/>
+          <section className="section-common section-report border-warning">
+            <article>
+              <h2 className="subtitle">
+                Sua opinião é importante!
+              </h2>
 
-          <p>
-            Caso queira reportar algum erro, sugestão ou qualquer problema, não tema! Basta apenas clickar no botão logo abaixo e reportar a questão.
-          </p>
-          <br/>
+              <p>
+                Caso queira reportar algum erro, sugestão ou qualquer problema, não tema! 
+              </p>
+              <p> 
+                Basta apenas clickar no botão logo abaixo e reportar a questão.
+              </p>
+            </article>
 
-          <button className="btn btn-warning">
-            Reportar!
-          </button>
-        </section>
-        <hr/>
-<br/><br/>
-      </main>
-      
-    </div>
+            <button className="btn btn-warning">
+               <i class="fi fi-rr-hexagon-exclamation"></i>  Reportar!
+            </button>
+          </section>
+          <br/><br/>
+        </main>
+
+      </div>
     </>
   );
 };
